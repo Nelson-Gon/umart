6e70b81d27ad6cb059da0d45c510ab9793cadf01
Nelson-Gon <gonzabato@hotmail.com>
2022-03-13 21:18:46 +0100

    New params

--- R/umart_main.R
+++ R/umart_main.R
@@ -16,10 +16,14 @@ generate_data <- function(seed=520, type = "norm", number = 50,
                           ...
 ){
 
-if(type == "norm"){
-  set.seed(seed)
-  rnorm(n = number, ...)
-}
+set.seed(seed)
+
+switch(type,
+       norm = rnorm(n = number, ...),
+       uniform = runif(n= number, ...),
+       poisson = rpois(n=number, ...)
+       )
+
 
 }
 
@@ -57,6 +61,10 @@ run_umap <- function(df=NULL, neighbors = NULL, seed = 520, ...){
 #' @param umap_df An object from run_umap
 #' @param opacity controls background opacity
 #' @param group_col Column to use to color UMAP result
+#' @param use_palette Specifies a palette to use for the coloring
+#' @param bg_color Specifies plot background color
+#' @param use_geom Species the kind of geom to use. Defaults to geom_path
+#' @importFrom methods getFunction
 #' @return UMAP art (umart)
 #' @export
 #' @examples
@@ -69,19 +77,26 @@ run_umap <- function(df=NULL, neighbors = NULL, seed = 520, ...){
 #' generate_umart(umap_df, group_col = r_df$group)
 #' @import ggplot2
 #' @importFrom rlang .data
-generate_umart <- function(umap_df, opacity = 0.25, group_col = NULL){
-  ggplot(umap_df,aes(.data[["x"]], .data[["y"]], fill=group_col))+
-    geom_area(alpha = opacity) +
+generate_umart <- function(umap_df, opacity = 0.25, group_col = NULL,
+                           use_palette = "Spectral",
+                           bg_color = "#b31c12",
+                           use_geom = "geom_path"
+                           ){
+  ggplot(umap_df,aes(.data[["x"]], .data[["y"]], fill=group_col,
+                     col = group_col))+
+    getFunction(use_geom)(alpha=opacity) +
     coord_polar() +
-    scale_fill_brewer(type = "div", palette = "Spectral") +
-    facet_wrap(~neighbors) +
+    scale_fill_brewer(type = "div", palette = use_palette) +
     theme_minimal() +
     theme(axis.title = element_blank(),
           axis.text = element_blank(),
           axis.ticks = element_blank(),
           panel.grid = element_blank(),
-          panel.background = element_rect(color="#f6feff",
-                                          fill = "#f6feff")
+          panel.background = element_rect(color=bg_color,
+                                          fill = bg_color),
+          plot.background = element_rect(color=bg_color,
+                                         fill = bg_color),
+          title = element_blank()
     ) +
-    guides(fill = "none")
+    guides(fill = "none", col="none")
 }
